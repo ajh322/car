6ebfb79ce3df7b8a8bd4865e5b39bf6f6ddeeb2f
AJH <jihwan322@gmail.com>
2017-03-11 16:06:30 +0900

    go

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ var app = express();
 var engine = require('ejs-locals');
 var mongoose = require('mongoose');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+
 mongoose.Promise = global.Promise;
 var conn = mongoose.createConnection('mongodb://35.161.80.18:27017/car');
 var Car = require('./models/car');
 app.set('port', process.env.PORT || 8080);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get('/', function (req, res) {
     conn.collection('car').insert({name: 2, user_id: "s"});
     res.end();
+});
+app.get('/test', function (req, res) {
+    res.render('test', {title:"title"});
 });
 app.listen(app.get('port'));
 

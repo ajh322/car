6b5fa6129db22cdc08b3641fd9266c54d9862303
AJH <jihwan322@gmail.com>
2017-03-11 17:14:49 +0900

    go

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 app.get('/test', function (req, res) {
     Car.findOne({}).exec(function (err, doc_l) {
         console.log(doc_l);
-        res.render('test', {data:data,length:data.length});
+        res.render('test', {data:doc_l,length:doc_l.length});
         res.end(JSON.stringify(doc_l));
     })
 });

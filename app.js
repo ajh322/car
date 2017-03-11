963e03d5d9fbf92016f87ca32a91d8a10f6a4241
ajh322 <jihwan322@gmail.com>
2017-03-11 08:20:53 +0000

    s

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 app.get('/test', function (req, res) {
     Car.find({}).exec(function (err, doc_l) {
         console.log(doc_l);
-        res.render('test', {data:doc_l,length:doc_l.length});
+//        res.render('test', {data:doc_l,length:doc_l.length});
         res.end(JSON.stringify(doc_l));
     })
 });

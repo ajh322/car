6fad601e9adbef75f942f1e2e44fd507fe0207dd
AJH <jihwan322@gmail.com>
2017-03-11 15:25:18 +0900

    go

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
-
+var mongoose = require('mongoose');
+var conn = mongoose.createConnection('mongodb://35.161.80.18:27017/car');
+var car = require('./models/car');
 // sets port 8080 to default or unless otherwise specified in the environment
 app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.sendFile(path.join(__dirname + '/test.html'));
 });
 
 // Only works on 3000 regardless of what I set environment port to or how I set

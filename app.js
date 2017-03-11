9805dec666a0b5eff1b1cf58889baecd5f694b14
AJH <jihwan322@gmail.com>
2017-03-11 16:02:15 +0900

    go

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ var path = require("path");
 var app = express();
 var engine = require('ejs-locals');
 var mongoose = require('mongoose');
+var fs = require('fs');
 mongoose.Promise = global.Promise;
 var conn = mongoose.createConnection('mongodb://35.161.80.18:27017/car');
 var Car = require('./models/car');
-// sets port 8080 to default or unless otherwise specified in the environment
-app.engine('ejs', engine);
 app.set('port', process.env.PORT || 8080);
 app.get('/', function (req, res) {
-    res.render('test', {title:"he",length:3});
+    conn.collection('car').insert({name: 2, user_id: "s"});
 });
-// Only works on 3000 regardless of what I set environment port to or how I set
-// [value] in app.set('port', [value]).
-// app.listen(3000);
 app.listen(app.get('port'));
 

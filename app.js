f6735c465555464085f04c7f0bc63c903652cbd3
AJH <jihwan322@gmail.com>
2017-03-23 00:46:02 +0900

    github auto deploy

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ app.get('/part_category', function (req, res) {
 });
 app.get('/getPartCategory', function (req, res) {
     part_category.find({}).exec(function (err, doc) {
-        res.end(doc);
+        console.log(doc);
+        res.render('test', {data: doc, length: doc.length});
     })
 });
 

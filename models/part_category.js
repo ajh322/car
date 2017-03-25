f63978cdc125f0e9e0ab6ba8df7c7168b7f8cf7f
AJH <jihwan322@gmail.com>
2017-03-25 16:13:39 +0900

    buildhtml file

--- models/part_category.js
+++ models/part_category.js
@@ -4,7 +4,7 @@
 var mongoose = require('mongoose');
 var conn = mongoose.createConnection('mongodb://35.161.80.18:27017/car');
 var userSchema = new mongoose.Schema({
-    part_category: {type: String, required: true, unique: true}
+    part_category: {type: String}
 });
 var part_category = conn.model('part_category', userSchema, "part_category");
 module.exports = part_category;

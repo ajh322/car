da47df4e12a954a631066d31052d841e8c51059c
AJH <jihwan322@gmail.com>
2017-04-07 23:23:06 +0900

    build file,img upload

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.get('/go_part_category', function (req, res) {
 });
 //파트 추가하기
 app.post('/add_part', upload_main.any(), function (req, res) {
-    console.log(req.files);
+    console.log("파일"+req.file);
     conn.collection('part').insert({part_category: req.body.part_category, part_name:req.body.part_name});
     part.find({part_category:req.body.part_category}).exec(function (err, doc) {
         console.log("파트 카테고리"+req.body.part_category);

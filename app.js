f9f823d85ded914c6c3c6f107ff1b52c7c12b843
AJH <jihwan322@gmail.com>
2017-04-11 02:27:29 +0900

    build file,img upload

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.get('/go_part_category', function (req, res) {
 var add_part_upload = upload_main.fields([{name: 'img', maxCount: 1}, {name: 'file', maxCount: 1}]);
 app.post('/add_part', add_part_upload, function (req, res, next) {
     //console.log("2" + req.file);
-    console.log("1" + req.files);
+    console.log("1" + req.files['img']);
     conn.collection('part').insert({part_category: req.body.part_category, part_name: req.body.part_name});
     part.find({part_category: req.body.part_category}).exec(function (err, doc) {
         console.log("파트 카테고리" + req.body.part_category);
